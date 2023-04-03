61c068f72ed0b7b84ac3c1339fe141db4a2cb9f9
Pradip Thapa <thapapradip542@gmail.com>
2023-04-03 15:30:53 +0545

    update the code

--- lazy-py-crawler/test/test.py
+++ lazy-py-crawler/test/test.py
@@ -1,71 +1,4 @@
 
-# LazyPyProcessor
-
-Customized Scrapy is a modified version of the popular open-source web scraping framework, Scrapy, that is designed for easy of setup and use. It includes a predefined library of functions and utilities that make it easy to extract and process data from websites.
-
-##### Note. This library was written for python3 and scrapy1.6.0. However higher version is supported.
-
-### Installation Instruction
-
-Create a virtual environment for your project. Install Customized Scrapy using pip 
-    
-```
-pip install git+https://github.com/Pradip-p/lazy-py-processor.git
-
-```
-or Alternatively, you can install LazyPyProcessor Scrapy by cloning the repository and installing it locally:
-```
-git clone https://github.com/Pradip-p/lazy-py-processor.git
-```
-```
-cd lazy-py-processor
-```
-```
-pip install . 
-```
-
-
-###### Note if you are having installation issue. please check if you have added your public ssh keys to github. Visit this blog for more details on adding ssh keys to github
-[https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account)
-
-### Visit documentation here
-
-#### It is recommended to take a look at scrapy documentation also as this library merly hides setup complexity of scrapy and some other settings. You would still need to learn scrapy framework for using spiders.
-
-https://docs.scrapy.org/en/latest/
-
-
-## Build instructions
-
-We use semantic versioning(https://en.wikipedia.org/wiki/Software_versioning)
-
-In order to build the docker container.
-
-1. Commit your changes.
-2. Increase the version(patch, minor , major)
-
-    Normally, its patch
-    
-    Install bumpversion for easier version management.
-    
-    In order to increase patch version simply do
-    `bumpversion patch`
-    
-    For example if the current tag is 1.04
-    Doing `bumpversion patch` will make the tag 1.05
-    
-3. push to tags
-```git push --tags```
-
-4. Also push to master branch
-``` git push origin master```
-
-### Usage
-### To use LazyPyProcessor 
-##### make a python file for your project (example: `scrapy_example.py`)
-
-
-```
 import os
 import scrapy
 from scrapy.crawler import CrawlerProcess
@@ -148,14 +81,3 @@ os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
 process = CrawlerProcess(get_project_settings())  
 process.crawl(LazyCrawler)
 process.start() # the script will block here until the crawling is finished
-```
-## Further resources
-
-For more information and examples of how to use Lazy Py Processor, see the project documentation.
-## Credits
-
-Lazy Py Processor was created by Pradip p.
-
-## License
-
-Lazy Py Processor is released under the MIT License.
